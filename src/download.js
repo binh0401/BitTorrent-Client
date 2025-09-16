43ef8d0e7f161675db34966ee3e64e79856da448
Binh <binh412005@gmail.com>
2025-09-16 01:06:42 -0400

    some handlers to download pieces

--- src/download.js
+++ src/download.js
@@ -1,16 +1,19 @@
 import net from "net"
 import { Buffer } from "buffer"
 import {getPeers} from "./tracker.js"
-import {buildHandshake, buildInterested} from './message.js'
+import {buildHandshake, buildInterested, buildRequest, messageParser} from './message.js'
 
 export default downloadFromPeers = (torrent) => {
+
+  const requested = [] //storing all pieces we have requested
+
   //Get list of peers, then download from each of them
   getPeers(torrent, peers => {
-    peers.forEach(peer => download(peer, torrent))
+    peers.forEach(peer => download(peer, torrent, requested))
   })
 }
 
-const download = (peer, torrent) => {
+const download = (peer, torrent, requested) => {
   //Create a TCP connection with each peer
   const socket = new net.Socket()
   socket.on("error", console.log)
@@ -21,12 +24,63 @@ const download = (peer, torrent) => {
   })
 
   //Handle responses from peer
-  onWholeMsg(socket, msg => msgHandler(msg, socket))
+  const queue = []
+  onWholeMsg(socket, msg => msgHandler(msg, socket, requested, queue))
 }
 
-const msgHandler = (msg, socket) => {
-  if(isHandshake(msg)) socket.write(buildInterested())
+const msgHandler = (msg, socket, requested, queue) => {
+  if(isHandshake(msg)){
+    socket.write(buildInterested())
+  }else{
+    const m = messageParser(msg)
+
+    if(m.id === 0) chokeHandler()
+    if(m.id === 1) unChokeHandler()
+    if(m.id === 4) haveHandler(m.payload, socket, requested, queue)
+    if(m.id === 5) bitfieldHandler(m.payload)
+    if(m.id === 7) pieceHandler(m.payload, socket, requested, queue)
+  }
 }
+
+const chokeHandler = () => {
+
+}
+
+const unChokeHandler = () => {
+
+}
+
+const haveHandler = (payload, socket, requested, queue) => {
+  const pieceIndex = payload.readUInt32BE(32)
+  queue.push(pieceIndex)
+  if(queue.length === 1){
+    requestPiece(socket, requested, queue)
+  }
+  if(!requested[pieceIndex]){
+    socket.write(buildRequest())
+  }
+  requested[pieceIndex] = true
+}
+
+const bitfieldHandler = (payload) => {
+
+}
+
+const pieceHandler = (payload, socket, requested, queue) => {
+
+  queue.shift()
+  requestPiece(socket, requested, queue)
+}
+
+const requestPiece = (socket, requested, queue) => {
+  if(requested[queue[0]]){
+    queue.shift()
+  }else{
+    socket.write(buildRequest(pieceIndex))
+  }
+}
+
+
 
 const isHandshake = (msg) => {
   return msg.length === msg.readUInt8(0) + 49 && msg.toString("utf8", 1) === "BitTorrent protocol"
--- src/message.js
+++ src/message.js
@@ -28,6 +28,25 @@ export const buildHandshake = (torrent) => {
 //NORMAL MESSAGE AFTER HANDSHAKE
 export const buildKeepAlive = () => Buffer.alloc(4)
 
+export const messageParser = (msg) => {
+  const id = msg.length > 4 ? msg.readInt8(0) : null
+  let payload = msg.length > 5 ? msg.slice(5) : null
+
+  if(id===6 || id === 7 || id === 8){
+    payload = {
+      index: payload.readUInt32BE(0),
+      begin: payload.readUInt32BE(4)
+    }
+  }
+  payload[id === 7 ? "block" : "length"] = rest
+
+  return {
+    size: msg.readInt32BE(0),
+    id,
+    payload
+  }
+}
+
 
 export const buildChoke = () => {
   const buffer = Buffer.alloc(5)
